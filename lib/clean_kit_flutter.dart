9b03fb1762a6791fe1d7e27903afa620a0eebea1
devnizamuddin <dev.nizamuddin@gmail.com>
2025-11-12 13:11:38 +0600

    first commit

---
+++ lib/clean_kit_flutter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CleanKitFlutter extends StatelessWidget {
+  const CleanKitFlutter({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
