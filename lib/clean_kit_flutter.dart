f96af7d041dac9f20a9318968fd126f9b1f144e9
devnizamuddin <dev.nizamuddin@gmail.com>
2025-11-16 12:05:38 +0600

    🚀 Add: starting clean flutter

--- lib/clean_kit_flutter.dart
+++ lib/clean_kit_flutter.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/router/app_router.dart';
+import 'core/theme/app_theme.dart';
+import 'features/login/bloc/auth_bloc.dart';
+import 'mockup/mock_login_use_case.dart';
 
 class CleanKitFlutter extends StatelessWidget {
-  const CleanKitFlutter({super.key});
+  final MockLoginUseCase loginUseCase;
+
+  const CleanKitFlutter({
+    super.key,
+    required this.loginUseCase,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BlocProvider(
+      //* Start event to check initial status
+      create: (context) => AuthBloc(loginUseCase)..add(AuthAppStarted(),),
+      child: Builder(
+        builder: (context) {
+          final router = AppRouter(context.read<AuthBloc>()).router;
+          return MaterialApp.router(
+            theme: AppTheme.lightData,
+            routerConfig: router,
+            builder: (context, child) {
+              final authState = context.watch<AuthBloc>().state;
+
+              if (authState is AuthInitial) {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+              return child ?? Container();
+            },
+          );
+        },
+      ),
+    );
   }
 }
